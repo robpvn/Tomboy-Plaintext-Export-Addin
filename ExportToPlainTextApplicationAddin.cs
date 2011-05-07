ec625f067fc1c14068cc2f92bcd3174e921075d8
Robert Nordan <rpvn@robpvn.net>
2011-05-07 23:42:00 +0200

    Code cleanup, nothing more should need to change here.

--- ExportToPlainTextApplicationAddin.cs
+++ ExportToPlainTextApplicationAddin.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.IO;
 using Tomboy;
@@ -6,46 +5,37 @@ using Mono.Unix;
 
 namespace Tomboy.ExportToPlainText
 {
-
-
 	public class ExportToPlainTextApplicationAddin : ExportAllApplicationAddin
 	{
-			
+		
 		protected override void setNames ()
 		{
 			export_file_suffix = "txt";
 			export_type_full_name = Catalog.GetString ("Plain Text Files");
 		}
 		
-		/// <summary>
-		/// Exports a single Note to HTML in a specified location. Returns an error text
-		/// if something goes wrong, otherwise null is returned.
-		/// </summary>
 		public override void ExportSingleNote (Note note, 
-		                                string output_path)
+													string output_path)
 		{
-			Logger.Debug (Catalog.GetString("Exporting Note '{0}' to '{1}'..."), note.Title, output_path);
-
+			Logger.Debug (Catalog.GetString("Exporting Note '{0}' to '{1}'..."),
+							note.Title, output_path);
+			
 			StreamWriter writer = null;
-
-
+			
 			try {
 				// FIXME: Warn about file existing.  Allow overwrite.
 				File.Delete (output_path);
 			} catch {
 			}
-
+			
 			writer = new StreamWriter (output_path);
 			
 			writer.Write(note.TextContent);
-	
-
+			
 			if (writer != null)
 				writer.Close ();
-
+			
 			return;
 		}
 	}
-	
-	
 }
